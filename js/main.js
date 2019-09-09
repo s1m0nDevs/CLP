359956599705643c30f977242f557a4424fe3207
Nikita <s1m0nkyivstar@gmail.com>
2019-09-09 15:35:34 +0300

    off preloader

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-function loadData() {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, 500);
-  })
-}
+// function loadData() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(resolve, 500);
+//   })
+// }
 
-loadData()
-  .then(() => {
-    let preloaderEl = document.getElementById('preloader');
-    preloaderEl.classList.add('hidden');
-    preloaderEl.classList.remove('visible');
-  });
+// loadData()
+//   .then(() => {
+//     let preloaderEl = document.getElementById('preloader');
+//     preloaderEl.classList.add('hidden');
+//     preloaderEl.classList.remove('visible');
+//   });
 
 window.addEventListener('beforeunload', (e)=>{window.scroll(0,0);})
 
